c1b14de540e6b3531171fbaac710af54b88e1a37
Tomasz Tomecki <tomasz.tomecki@jcommerce.pl>
2018-06-09 08:45:02 +0200

    added swagger to branch

--- src/main/java/pl/toto/worldcup/domain/teams/TeamController.java
+++ src/main/java/pl/toto/worldcup/domain/teams/TeamController.java
@@ -1,5 +1,7 @@
 package pl.toto.worldcup.domain.teams;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +12,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
+@Api("Operations for teams")
 class TeamController {
 
     private final TeamService teamService;
 
+    @ApiOperation("Find all teams")
     @GetMapping
     List<TeamDto> teams() {
         return teamService.getTeams();
